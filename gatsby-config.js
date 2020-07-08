5af2277053fb912072b11201cc0eaf58e7952ca8
Brian Reynolds <brian@growratio.com>
2020-07-08 10:08:46 -0400

    preloading fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,14 +27,17 @@ module.exports = {
         icon: `src/images/br-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: "gatsby-plugin-web-font-loader",
-      options: {
-        google: {
-          families: ["Roboto"],
-        },
-      },
-    },
+    // LOAD FONTS VIA GATSBY PLUGIN
+    // CURRENTLY DISABLED. LOADING THROUGH gatsby-ssr.js
+    // SO THAT FONTS CAN BE PRELOADED
+    // {
+    //   resolve: "gatsby-plugin-web-font-loader",
+    //   options: {
+    //     google: {
+    //       families: ["Roboto"],
+    //     },
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,20 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react")
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      key="preload-roboto-font"
+      rel="preload"
+      as="style"
+      href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+    />,
+    <link
+      key="roboto-font"
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+    />,
+  ])
+}
